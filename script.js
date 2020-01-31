a352942120c5e8cb292f23ed853ed132b2e37e53
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-01-30 23:32:08 -0700

    getting a lot of the bootstrap done, got the slide show to work, planning on adding the social-media later

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-var images = ['images/flopslap-1', 'images/flopslap-2', 'images/flopslap-3', 'images/flopslap-4', 'images/flopslap-5', 'images/flopslap-6',
-    'images/flopslap-7', 'images/flopslap-8', 'images/flopslap-9', 'images/flopslap-10', 'images/flopslap-11', 'images/flopslap-12', 'images/flopslap-13',
-    'images/flopslap-14', 'images/flopslap-15', 'images/flopslap-16', 'images/flopslap-17', 'images/flopslap-18', 'images/flopslap-19', 'images/flopslap-20',
-    'images/flopslap-21', 'images/flopslap-22', 'images/flopslap-23', 'images/flopslap-24', 'images/flopslap-25', 'images/flopslap-26']
-var nextimage = 0;
-
-doSlideshow();
-
-function doSlideshow() {
-    if (nextimage >= images.length) { nextimage = 0; }
-    $("#body")
-        .css('background-image', 'url("' + images[nextimage++] + '")')
-        .fadeIn(500, function () {
-            setTimeout(doSlideshow, 1000);
-        });
-}
+$('.carousel').carousel({
+    interval: 10000,
+    pause: false
+})
