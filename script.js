980f93988548acc4e9a049d741af42941d530b37
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-02-03 16:50:15 -0700

    adding a jumbotron to the homepage, trying a new approach to the background image but it doesnt work yet, mostly mobile friendly

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-$('.carousel').carousel({
-    interval: 10000,
-    pause: false
-})
+let imageNumber = 1;
+const images = new Array(
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
+    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`);
+
+images.forEach(element => {
+    let el = element;
+    $("#body").css('background-image', 'url("' + el + '")');
+    $("#body").css("background-size", "cover");
+    imageNumber++;
+}, 100);
