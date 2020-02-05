33718ad3ca770e9924d1468162eee54fd30841bb
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-02-05 10:58:52 -0700

    still working on slideshow background

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let imageNumber = 1;
-const images = new Array(
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`,
-    `images/flopslap-` + `${imageNumber}` + `.png`, `images/flopslap-` + `${imageNumber}` + `.png`);
+var imageNumber = 0;
+const images = [
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`];
 
-images.forEach(element => {
-    let el = element;
-    $("#body").css('background-image', 'url("' + el + '")');
-    $("#body").css("background-size", "cover");
-    imageNumber++;
-}, 100);
+// setInterval(function () {
+//     $("#body").css('background-image', 'url("' + images[imageNumber] + '")');
+//     $("#body").css("background-size", "cover");
+//     imageNumber = imageNumber + 1;
+//     console.log(imageNumber);
+//     if (imageNumber == images.length) {
+//         imageNumber = 0;
+//     }
+// }, 1000);
+
+setInterval(function () {
+    var b = $('#body');
+    b.css("background-image", "url(" + images[imageNumber] + ")");
+    b.css("background-size", "cover");
+    imageNumber = imageNumber + 1;
+    console.log(images[imageNumber])
+    console.log(imageNumber);
+    if (imageNumber >= images.length) { imageNumber = 0 }
+}, 1000);
