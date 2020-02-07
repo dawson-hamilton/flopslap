4e0ab4b13d309c01f4bfe392844ff7eca334f492
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-02-06 18:26:36 -0700

    finally getting the slide show to work properly but i still need to get the slideshow to fade better and there are still some little things that need to be worked on

--- script.js
+++ script.js
@@ -10,22 +10,10 @@ const images = [
     `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
     `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`];
 
-// setInterval(function () {
-//     $("#body").css('background-image', 'url("' + images[imageNumber] + '")');
-//     $("#body").css("background-size", "cover");
-//     imageNumber = imageNumber + 1;
-//     console.log(imageNumber);
-//     if (imageNumber == images.length) {
-//         imageNumber = 0;
-//     }
-// }, 1000);
-
 setInterval(function () {
     var b = $('#body');
-    b.css("background-image", "url(" + images[imageNumber] + ")");
+    b.css("background-image", "url(images/flopslap-" + imageNumber + ".png)");
     b.css("background-size", "cover");
-    imageNumber = imageNumber + 1;
-    console.log(images[imageNumber])
-    console.log(imageNumber);
+    imageNumber++;
     if (imageNumber >= images.length) { imageNumber = 0 }
-}, 1000);
+}, 10000);
