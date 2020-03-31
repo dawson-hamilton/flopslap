219449178ea2e1dbb19e797a468817fb193ac04e
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-03-31 16:25:29 -0600

    making the pictures transition slower and making everything line up correctly

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var imageNumber = 0;
+var imageNumber = 1;
 const images = [
     `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
     `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
@@ -8,12 +8,17 @@ const images = [
     `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
     `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
     `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`,
-    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`];
+    `images/flopslap-` + imageNumber + `.png`, `images/flopslap-` + imageNumber + `.png`
+];
 
 setInterval(function () {
     var b = $('#body');
-    b.css("background-image", "url(images/flopslap-" + imageNumber + ".png)");
-    b.css("background-size", "cover");
-    imageNumber++;
+    $("#body").fadeOut(400, function () {
+        b.attr("src", "images/flopslap-" + imageNumber + ".png");
+    })
+        .fadeIn(400, function () {
+            b.attr("src", "images/flopslap-" + imageNumber + ".png")
+            imageNumber++;
+        });
     if (imageNumber >= images.length) { imageNumber = 0 }
 }, 10000);
